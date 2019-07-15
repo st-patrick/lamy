e64ed223d389eeef1e5d353d963e4507ae36cc98
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2019-07-14 22:09:17 -0700

    added obstacle block and changed background color for now

--- ProjectGood/Rect.h
+++ ProjectGood/Rect.h
@@ -7,15 +7,10 @@ public:
 	//constructor for colored rectangle
 	Rect(const Window &window, int w, int h, int x, int y, int r, int g, int b, int a);
 
-	//constructor for image rectangle
-	Rect(const Window &window, int w, int h, int x, int y, const std::string &img);
-
 	void draw() const;//draw function to display rectangles
 	void pollEvents(SDL_Event &event);//handle rectangle events
 
 private:
 	//variables for width, height, position, and color
 	int m_w, m_h, m_x, m_y, m_r, m_g, m_b, m_a;
-	//texture to be used for image later when it works
-	SDL_Texture* m_texture = nullptr;
 };
