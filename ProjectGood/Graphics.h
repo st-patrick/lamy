a86f7b70b7848f3e5921254464bd4a245fa904ce
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2019-07-19 23:56:28 -0700

    this is currently not working, but I am working on this

--- ProjectGood/Graphics.h
+++ ProjectGood/Graphics.h
@@ -3,19 +3,18 @@
 #include<string>
 #include<SDL_image.h>
 
-class Graphics : public Rect //Graphics inherits from Rect, which inherits from Window
+class Graphics : public Window //Graphics inherits from Rect, which inherits from Window
 {
 public:
-	Graphics(const Rect &rect, const std::string& path);//constructor
+	Graphics(Window window, const std::string& path);//constructor
 	~Graphics();//destructor
 
-	void Render(SDL_Renderer* renderer) const;
+	void Render(Window window) const;
 
 private:
-	SDL_Rect m_destRect;//destination rectangle
-	SDL_Texture* m_texture;//texture for texture from surface
-	std::string m_path;
 
 protected:
+	SDL_Rect* m_destRect;//destination rectangle
+	SDL_Texture* m_texture;//texture for texture from surface
 
 };
