f8b8819c8213f488a4e014f90ec1b77e74235335
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2019-07-19 23:57:26 -0700

    this is currently not working, but I am working on this

--- ProjectGood/main.cpp
+++ ProjectGood/main.cpp
@@ -2,6 +2,7 @@
 #include<SDL.h>//include SDL funcitons
 #include"Window.h"//include window header file for window functions
 #include"Rect.h"//include rect header file for rect functions
+#include"Graphics.h"
 
 //allow events for the window and rectangle separately
 //doesn't allow either to cancel the other out like before
@@ -16,11 +17,14 @@ void pollEvents(Window& window, Rect& rect) {
 //main function
 int main(int argc, char* argv[]) {
 	Window window("Good Code", 800, 450);
+	Rect bgdestination(window, 800, 450, 0, 0, 0, 0, 0, 255);
 	Rect rect(window, 10, 10, 10, 10, 255, 0, 0, 255);
 	Rect obstacle(window, 100, 100, 350, 175, 0, 0, 0, 255);
+	Graphics bgimage(bgdestination, "images/background.png");
 
 	while (!window.isClosed()) {//while the program is running
 		pollEvents(window, rect);//allows events for window and square
+		bgimage.Render(window);
 		rect.draw();//draw the square
 		obstacle.draw();
 		window.clear();//render graphics
