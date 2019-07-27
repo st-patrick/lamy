f9dd7e1d84a5e018ba394d51df42f3bc3791ba6f
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2019-07-27 13:04:16 -0700

    I got the sketch to load in the background as a .jpg image, and I started working on the tile map.

---
+++ tiles/paintedTiles.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="paintedTiles" tilewidth="20" tileheight="15" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="20" height="15" source="tile1.jpg"/>
+ </tile>
+ <tile id="1">
+  <image width="20" height="15" source="tile2.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="20" height="15" source="tile3.jpg"/>
+ </tile>
+ <tile id="3">
+  <image width="20" height="15" source="tile4.jpg"/>
+ </tile>
+ <tile id="4">
+  <image width="20" height="15" source="tile5.jpg"/>
+ </tile>
+ <tile id="5">
+  <image width="20" height="15" source="tile6.jpg"/>
+ </tile>
+ <tile id="6">
+  <image width="20" height="15" source="tile7.jpg"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="15" source="tile8.jpg"/>
+ </tile>
+ <tile id="8">
+  <image width="20" height="15" source="tile9.jpg"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="15" source="tile10.jpg"/>
+ </tile>
+ <tile id="10">
+  <image width="20" height="15" source="tile11.jpg"/>
+ </tile>
+ <tile id="11">
+  <image width="20" height="15" source="tile12.jpg"/>
+ </tile>
+ <tile id="12">
+  <image width="20" height="15" source="tile13.jpg"/>
+ </tile>
+ <tile id="13">
+  <image width="20" height="15" source="tile14.jpg"/>
+ </tile>
+ <tile id="14">
+  <image width="20" height="15" source="tile15.jpg"/>
+ </tile>
+</tileset>
