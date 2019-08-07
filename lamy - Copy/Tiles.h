bb297a54113151797b675a1fa1773aad5b7d8901
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2019-08-06 21:11:31 -0700

    I started from the last working project (which is with the image in the background and the square moving but unable to move through the obstacle), and I am working on the tile map and getting everything to do what it is supposed to do. I am working on the methods to get everything started, and I will put them together to make it run soon.

---
+++ lamy - Copy/Tiles.h
@@ -0,0 +1,39 @@
+#pragma once
+#include"Window.h"//include Window for inheritance
+
+class Tiles : public Window {
+public:
+
+	//constructor
+	Tiles(const Window& window, int x, int y, int type);
+
+protected:
+	//tile position variables
+	int mx, my, mtype;
+
+	//accessibility
+	bool mlocked;//true for locked, false for accessible
+
+	//collision box
+	SDL_Rect mbox;
+
+	//return tile information
+	int getType();//tile type
+	SDL_Rect getBox();//collision box
+	bool getStatus();//accessible or not
+
+	//dimensions of tiles
+	const int TILE_WIDTH = 32;
+	const int TILE_HEIGHT = 32;
+
+	//dimensions of screen
+	const int SCREEN_WIDTH = m_width;
+	const int SCREEN_HEIGHT = m_height;
+
+	//tile types
+	const int OPEN = 0;
+	const int OPEN_DOOR = 1;
+	const int CLOSED_DOOR = 2;
+	const int WALL = 3;
+
+};
---
+++ lamy/rect.cpp
@@ -0,0 +1,87 @@
+#include "Rect.h"//include wrap header file (wrapper class for sprite and tiles)
+#include <SDL_image.h>
+#include<iostream>//allow console output
+
+//Rect constructor for use with a color
+Rect::Rect(const Window& window, int w, int h, int x, int y, int r, int g, int b, int a) :
+	Window(window), m_w(w), m_h(h), m_x(x), m_y(y), m_r(r), m_g(g), m_b(b), m_a(a)
+{
+
+}
+
+//Draw function draws the rectangle when called
+void Rect::draw() const {
+	//draw colored square
+	SDL_Rect rect = { m_x, m_y, m_w, m_h };
+	SDL_SetRenderDrawColor(m_renderer, m_r, m_g, m_b, m_a);
+	SDL_RenderFillRect(m_renderer, &rect);
+}
+
+//allows the square to move around the window and not go through obstacle block
+void Rect::pollEvents(SDL_Event& event) {
+
+	switch (event.type) {
+	case SDL_KEYDOWN:
+		switch (event.key.keysym.sym) {
+		case SDLK_LEFT:
+			std::cout << "Left arrow pressed" << std::endl;
+			if ((m_x - 5) <= 0) {
+				m_x = 0;//doesn't let the square leave the window
+			}
+			else if (m_y < 275 && m_y > 165 && (m_x - 5) <= 450 && m_x >= 450) {
+				m_x = 450;
+			}
+			else {
+				m_x -= 5;//allows the square to move
+			}
+			break;
+		case SDLK_RIGHT:
+			std::cout << "Right arrow pressed" << std::endl;
+			if (m_x + 5 >= 790) {
+				m_x = 790;//doesn't let the square leave the window
+			}
+			else if (m_y < 275 && m_y > 165 && (m_x + 5) >= 340 && m_x <= 340) {
+				m_x = 340;
+			}
+			else {
+				m_x += 5;//allows the square to move
+			}
+			break;
+		case SDLK_DOWN:
+			std::cout << "Down arrow pressed" << std::endl;
+			if (m_y + 5 >= 440) {
+				m_y = 440;//doesn't allow the square to leave window
+			}
+			else if (m_x > 340 && m_x < 450 && (m_y + 5) >= 165 && m_y <= 165) {
+				m_y = 165;
+			}
+			else {
+				m_y += 5;//allows square to move
+			}
+			break;
+		case SDLK_UP:
+			std::cout << "Up arrow pressed" << std::endl;
+			if (m_y - 5 <= 0) {
+				m_y = 0;//doesn't allow square to leave window
+			}
+			else if (m_x > 340 && m_x < 450 && (m_y - 5) <= 275 && m_y >= 275) {
+				m_y = 275;
+			}
+			else {
+				m_y -= 5;//allows square to move
+			}
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+
+}
+
+//check to see if it is colliding with tiles on tile map
+bool Rect::checkCollision(SDL_Rect a, SDL_Rect b) {
+
+}
